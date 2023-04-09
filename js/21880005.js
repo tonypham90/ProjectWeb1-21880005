e0ad385106255712266f2c88574ba86b40989e00
tonypham90 <tuananhdc08kt@gmail.com>
2023-04-09 15:54:10 +0700

    update handlebars for news

---
+++ js/21880005.js
@@ -0,0 +1,11 @@
+const API = 'https://web1-api.vercel.app/api';
+
+async function loadData(request, templateId,viewID) {
+        const response = await fetch(API.concat("/",request));
+        const data = await response.json();
+        var source = document.getElementById(templateId).innerHTML;
+        var template = Handlebars.compile(source);
+        var context = { data: data };
+        var view = document.getElementById(viewID);
+        view.innerHTML = template(context);
+      }
