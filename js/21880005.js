e415b206c884e457dbf9c6a76633b564103d57f8
tonypham90 <tuananh.gt@hotmail.com>
2023-04-28 15:17:32 +0700

    update verify

--- js/21880005.js
+++ js/21880005.js
@@ -1,5 +1,5 @@
-const API = 'https://web1-api.vercel.app/api';
-const AUTHENTICATE_API = 'https://web1-api.vercel.app/users'
+const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/users";
 
 async function loadData(request, templateId, viewID) {
   const response = await fetch(API.concat("/", request));
@@ -12,60 +12,76 @@ async function loadData(request, templateId, viewID) {
 }
 
 async function getAuthenticateToken(username, password) {
-  let response = await fetch(`${ AUTHENTICATE_API }/authenticate`, {
+  let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    }, body: JSON.stringify({ username, password })
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
   });
   let result = await response.json();
-  if (response.status = 200) {
+  if ((response.status = 200)) {
     return result.token;
   }
   throw new Error(result.message);
 }
 
-async function login(e){
+async function login(e) {
   e.preventDefault();
 
-  let username = document.getElementById('username').value;
-  let password = document.getElementById('password').value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  document.getElementById('errorMessage').innerHTML = '';
 
-  try{
-    let token = await getAuthenticateToken(username,password);
-    if (token){
-      localStorage.setItem('token', token);
-      document.getElementsByClassName('btn-close')[0].click();
+  try {
+    let token = await getAuthenticateToken(username, password);
+    if (token) {
+      localStorage.setItem("token", token);
+      document.getElementsByClassName("btn-close")[0].click();
       displayControls();
     }
-
-  }
-  catch(error){
-    document.getElementById('errorMessage').innerHTML = error;
+  } catch (error) {
+    document.getElementById("errorMessage").innerHTML = error;
     displayControls(false);
   }
 }
 
-function displayControls(isLogin = true){
-  let linkLogins = document.getElementsByClassName('linkLogin');
-  let linkLogouts = document.getElementsByClassName('linkLogout');
-  let displayLogin = 'none';
-  let displaylogout = 'block';
-  if(!isLogin){
-    displayLogin = 'block';
-    displaylogout = 'none';
+function displayControls(isLogin = true) {
+  let linkLogins = document.getElementsByClassName("linkLogin");
+  let linkLogouts = document.getElementsByClassName("linkLogout");
+  let displayLogin = "none";
+  let displaylogout = "block";
+  if (!isLogin) {
+    displayLogin = "block";
+    displaylogout = "none";
   }
 
-  for (let i=0; i<2; i++){
-    linkLogins.style.display = displayLogin;
-    linkLogouts.style.display = displaylogout;
+  for (let i = 0; i < 2; i++) {
+    linkLogins[i].style.display = displayLogin;
+    linkLogouts[i].style.display = displaylogout;
   }
-
-
 }
 
-async function checkLogin(){
-  let token = localStorage.getItem('token');
-  
+async function checkLogin() {
+  let isLogin = await verifyToken();
+  displayControls(isLogin)
+}
+
+async function verifyToken() {
+  let token = localStorage.getItem("token");
+  if (token) {
+    let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer" + token,
+      },
+    });
+    if (response.status == 200) {
+      return true;
+    }
+  }
+  return false;
 }
